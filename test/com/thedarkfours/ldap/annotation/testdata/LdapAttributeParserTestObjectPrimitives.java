309124fab354101f453e969372020a99d69d4b05
rene <rene@dagon>
2013-10-03 23:38:13 +0200

    - Added even more tests

---
+++ test/com/thedarkfours/ldap/annotation/testdata/LdapAttributeParserTestObjectPrimitives.java
@@ -0,0 +1,138 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 rene.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.thedarkfours.ldap.annotation.testdata;
+
+import com.thedarkfours.ldap.annotation.LdapAttribute;
+import com.thedarkfours.ldap.schema.LdapObject;
+import java.util.Collection;
+
+/**
+ *
+ * @author rene
+ */
+public class LdapAttributeParserTestObjectPrimitives implements LdapObject {
+
+    @LdapAttribute
+    long longValue;
+    @LdapAttribute
+    String stringValue;
+    @LdapAttribute
+    int integerValue;
+    @LdapAttribute
+    char charValue;
+    @LdapAttribute
+    boolean boolValue;
+    @LdapAttribute
+    float floatValue;
+    @LdapAttribute
+    double doubleValue;
+    @LdapAttribute
+    short shortValue;
+
+    @Override
+    public String getDn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<String> getObjectClass() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setDn(String dn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setObjectClass(Collection<String> objectClass) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public long getLongValue() {
+        return longValue;
+    }
+
+    public void setLongValue(long longValue) {
+        this.longValue = longValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public int getIntegerValue() {
+        return integerValue;
+    }
+
+    public void setIntegerValue(int integerValue) {
+        this.integerValue = integerValue;
+    }
+
+    public char getCharValue() {
+        return charValue;
+    }
+
+    public void setCharValue(char charValue) {
+        this.charValue = charValue;
+    }
+
+    public boolean getBoolValue() {
+        return boolValue;
+    }
+
+    public void setBoolValue(boolean boolValue) {
+        this.boolValue = boolValue;
+    }
+
+    public float getFloatValue() {
+        return floatValue;
+    }
+
+    public void setFloatValue(float floatValue) {
+        this.floatValue = floatValue;
+    }
+
+    public double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public void setDoubleValue(double doubleValue) {
+        this.doubleValue = doubleValue;
+    }
+
+    public short getShortValue() {
+        return shortValue;
+    }
+
+    public void setShortValue(short shortValue) {
+        this.shortValue = shortValue;
+    }
+
+}
