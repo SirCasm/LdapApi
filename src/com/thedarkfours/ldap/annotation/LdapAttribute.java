01ecd722e0fd216baf68a3071aa38d5e874a2f95
René Döbele <tenobok@yahoo.de>
2013-10-06 23:29:40 +0200

    - Renamed a few methods - Distinguished name and object class is now set correctly

--- src/com/thedarkfours/ldap/annotation/LdapAttribute.java
+++ src/com/thedarkfours/ldap/annotation/LdapAttribute.java
@@ -7,6 +7,7 @@
 package com.thedarkfours.ldap.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -15,6 +16,7 @@ import java.lang.annotation.Target;
  *
  * @author rene
  */
+@Inherited
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LdapAttribute {
