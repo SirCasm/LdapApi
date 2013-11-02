49d19375e83711e8ee2f174ec7a47322001209dd
rene <tenobok@yahoo.de>
2013-11-02 01:50:46 +0100

    - Added another test

--- src/test/java/com/thedarkfours/ldap/LdapBindInformationTest.java
+++ src/test/java/com/thedarkfours/ldap/LdapBindInformationTest.java
@@ -27,9 +27,6 @@ public class LdapBindInformationTest {
     public void testRemoveBaseDn() throws Exception {
         System.out.println("removeBaseDn");
         LdapName name = new LdapName("ou=test,dc=thedarkfours,dc=com");
-        System.out.println(name.get(0));
-        System.out.println(name.get(1));
-        System.out.println(name.get(2));
         LdapBindInformation instance = new LdapBindInformation("ldap://", new LdapName("dc=thedarkfours,dc=com"), "", "");
         instance.removeBaseDn(name);
 
@@ -41,14 +38,32 @@ public class LdapBindInformationTest {
     public void testRemoveAnotherBaseDn() throws Exception {
         System.out.println("removeAnotherBaseDn");
         LdapName name = new LdapName("cn=blablub,ou=test,dc=thedarkfours,dc=com");
-        System.out.println(name.get(0));
-        System.out.println(name.get(1));
-        System.out.println(name.get(2));
         LdapBindInformation instance = new LdapBindInformation("ldap://", new LdapName("dc=thedarkfours,dc=com"), "", "");
         instance.removeBaseDn(name);
 
-        assertEquals(1, name.size());
+        assertEquals(2, name.size());
         assertEquals("cn=blablub,ou=test", name.toString());
     }
 
+    @Test
+    public void testRemoveBaseDnFromBaseDn() throws Exception {
+        System.out.println("removeBaseDnFromBaseDn");
+        LdapName name = new LdapName("dc=thedarkfours,dc=com");
+        LdapBindInformation instance = new LdapBindInformation("ldap://", new LdapName("dc=thedarkfours,dc=com"), "", "");
+        instance.removeBaseDn(name);
+
+        assertEquals(0, name.size());
+        assertEquals("", name.toString());
+    }
+
+    @Test
+    public void testRemoveBaseBaseDnNotIncluded() throws Exception {
+        System.out.println("removeBaseBaseDnNotIncluded");
+        LdapName name = new LdapName("dc=thedarkfarts,dc=com");
+        LdapBindInformation instance = new LdapBindInformation("ldap://", new LdapName("dc=thedarkfours,dc=com"), "", "");
+        instance.removeBaseDn(name);
+
+        assertEquals(2, name.size());
+        assertEquals("dc=thedarkfarts,dc=com", name.toString());
+    }
 }
